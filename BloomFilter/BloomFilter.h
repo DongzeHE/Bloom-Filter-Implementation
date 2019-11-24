c028d4806b015051b367435eae1b7e2ed2745bb5
Dongze He <32473855+zzaazzaa@users.noreply.github.com>
2019-11-24 13:07:10 -0500

    Add files via upload

---
+++ BloomFilter/BloomFilter.h
@@ -0,0 +1,27 @@
+// This is the head file of assignment 2.
+#include "pch.h"
+#include <array>
+#include <vector>
+#include <string>
+#include <list>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+
+class bf {
+public:
+    bf(int bvSize, int NumofHash);
+    int bvSize;
+    void insert( string keyValue);
+    bool query( string keyValue);
+    void printBV();
+    void updateBV(std::vector<bool> BV);
+    void writeOutput(string outFile_path);
+
+    int numHashes;
+    vector<bool> bv;
+};
---
+++ BloomFilter/pch.h
@@ -0,0 +1,8 @@
+//
+// Created by Dongze He on 11/17/19.
+//
+
+#ifndef ASSIGNMENT2_PCH_H
+#define ASSIGNMENT2_PCH_H
+
+#endif //ASSIGNMENT2_PCH_H
