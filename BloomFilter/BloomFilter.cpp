3d8ade4b0d6cbb4d75f2f3b4207aa4c0b8e47397
Dongze He <32473855+zzaazzaa@users.noreply.github.com>
2019-11-24 13:42:51 -0500

    Update BloomFilter.cpp

--- BloomFilter/BloomFilter.cpp
+++ BloomFilter/BloomFilter.cpp
@@ -1,8 +1,8 @@
 // This is the main function of assignment 2.
 //#include "pch.h"
 #include "BloomFilter.h"
-#include "smhasher-master/src/MurmurHash3.h"
-#include "smhasher-master/src/MurmurHash3.cpp"
+#include "MurmurHash3.h"
+#include "MurmurHash3.cpp"
 #define LOG(x) std::cout<< std::endl << "LOG: "<< x << std::endl<< std::endl;
 
 using namespace std;
